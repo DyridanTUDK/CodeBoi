37c769422efc98d488708c35956c0635047c69f4
dyridantudk <dyridantudK@gmail.com>
2020-10-10 08:03:44 +0600

    Updates

---
+++ precache-manifest.89b63a73067cc5c4647798ed720aa8e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1faa545602bb9d883f22ae8b32a2c9",
+    "url": "/CodeBoi/index.html"
+  },
+  {
+    "revision": "6044c2a02469e9ed3760",
+    "url": "/CodeBoi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6305e7f856eb118aa602",
+    "url": "/CodeBoi/static/css/main.d27c6434.chunk.css"
+  },
+  {
+    "revision": "6044c2a02469e9ed3760",
+    "url": "/CodeBoi/static/js/2.4a2bb028.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/CodeBoi/static/js/2.4a2bb028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6305e7f856eb118aa602",
+    "url": "/CodeBoi/static/js/main.9b5c740e.chunk.js"
+  },
+  {
+    "revision": "a1b39e6c5fba03830e9c",
+    "url": "/CodeBoi/static/js/runtime-main.52ce8344.js"
+  }
+]);

20de9405300ccbfbe42609f1dd53454c3bede5c5
dyridantudk <dyridantudK@gmail.com>
2020-10-11 08:57:28 +0600

    Updates

---
+++ precache-manifest.3bd245513319580ff3bae5feff1dbdab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896d70ca4232d9cbd608ee590a1fc127",
+    "url": "/CodeBoi/index.html"
+  },
+  {
+    "revision": "6044c2a02469e9ed3760",
+    "url": "/CodeBoi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c0b445fff9187310193a",
+    "url": "/CodeBoi/static/css/main.d27c6434.chunk.css"
+  },
+  {
+    "revision": "6044c2a02469e9ed3760",
+    "url": "/CodeBoi/static/js/2.4a2bb028.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/CodeBoi/static/js/2.4a2bb028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b445fff9187310193a",
+    "url": "/CodeBoi/static/js/main.1bd6a693.chunk.js"
+  },
+  {
+    "revision": "a1b39e6c5fba03830e9c",
+    "url": "/CodeBoi/static/js/runtime-main.52ce8344.js"
+  }
+]);
